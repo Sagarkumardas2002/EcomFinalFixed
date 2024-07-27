8e0dbc130a954457bb1beafb37fb25ca338fcdef
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2024-07-28 02:28:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ connectDB();
 const app = express();
 
 //middlewares
-// app.use(cors());
-app.use(cors({
-  origin: 'https://ecom-final-fixed.vercel.app/'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
